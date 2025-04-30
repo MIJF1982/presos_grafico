be74d85b8d569dd4a24cf8110a7ba2ade7a185e2
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-04-30 19:14:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Dashboard from "./pages/Dashboard";
+import { firebaseConfig } from "./services/firebase";
 
-export default function App() {
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => onAuthStateChanged(auth, u => setUser(u)), []);
   return (
-    <div>
-      <h1>Bem-vindo ao Presos Gráfico!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/dashboard" /> : <LoginPage auth={auth} />} />
+        <Route path="/signup" element={user ? <Navigate to="/dashboard" /> : <SignupPage auth={auth} firebaseApp={app}/>} />
+        <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
+export default App;
