1c0c16ecdf5d610d153b3d22b503f47873e0e669
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-04-30 21:16:17 +0100

    Update RealTimeChart.jsx

--- src/components/RealTimeChart.jsx
+++ src/components/RealTimeChart.jsx
@@ -10,16 +10,11 @@ export default function RealTimeChart({ symbol }) {
   useEffect(() => {
     chartRef.current = createChart(ref.current, { width: ref.current.clientWidth, height: 300 });
     seriesRef.current = chartRef.current.addCandlestickSeries();
-
     const socket = new WebSocket(TD_WEBSOCKET_URL(symbol));
     socket.onmessage = e => {
       const msg = JSON.parse(e.data);
-      if (msg.price) {
-        const time = Math.floor(Date.now() / 1000);
-        seriesRef.current.update({ time, open: msg.price, high: msg.price, low: msg.price, close: msg.price });
-      }
+      if (msg.price) seriesRef.current.update({ time: Math.floor(Date.now()/1000), open: msg.price, high: msg.price, low: msg.price, close: msg.price });
     };
-
     const onResize = () => chartRef.current.applyOptions({ width: ref.current.clientWidth });
     window.addEventListener("resize", onResize);
     return () => { window.removeEventListener("resize", onResize); socket.close(); };
