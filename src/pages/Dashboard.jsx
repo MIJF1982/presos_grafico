9e58a49e490a8db8c7b10b9ba681c1bebb1003a7
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-04-30 21:15:24 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react";
 import RealTimeChart from "../components/RealTimeChart";
 import Watchlist from "../components/Watchlist";
 import { db } from "../services/firebase";
-import { collection, doc, onSnapshot } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 
 export default function Dashboard({ user }) {
   const [symbols, setSymbols] = useState([]);
-
   useEffect(() => {
     const unsub = onSnapshot(doc(db, "users", user.uid), snap => {
       setSymbols(snap.data()?.watchlist || []);
@@ -15,12 +14,11 @@ export default function Dashboard({ user }) {
   }, [user.uid]);
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Bem-vindo, {user.email}</h1>
+    <div>
       <Watchlist symbols={symbols} user={user} />
       {symbols.map(sym => (
-        <div key={sym} style={{ marginTop: 40 }}>
-          <h2>Gráfico: {sym}</h2>
+        <div key={sym} style={{ marginTop: 20 }}>
+          <h2>{sym}</h2>
           <RealTimeChart symbol={sym} />
         </div>
       ))}
