5f8ad4a25f58922dd766fff2283dc91520f430ad
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-04-30 16:03:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-```js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.css"; // caso tenha um arquivo CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-```
