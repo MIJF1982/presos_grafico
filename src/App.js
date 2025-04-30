82a59a620403d20d5bce57a9de371c29a0286b3f
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-04-30 18:43:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div>
       <h1>Bem-vindo ao Presos Gráfico!</h1>
     </div>
   );
 }
-
-export default App;
