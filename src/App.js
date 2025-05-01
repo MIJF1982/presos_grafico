a15c8ed7dce7a162e60ac90ecc6edcf1ecfd2a21
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-05-01 11:27:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  const isAuthenticated = !!localStorage.getItem('auth');
-
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />} />
-        <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
-
