4f7a57552c9746bbc422df6b58919053000b7ff5
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-05-01 10:55:03 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from "react";
-import RealTimeChart from "../components/RealTimeChart";
-import Watchlist from "../components/Watchlist";
-import { db } from "../services/firebase";
-import { doc, onSnapshot } from "firebase/firestore";
+import React from 'react';
+import TradingViewChart from '../components/TradingViewChart';
 
-export default function Dashboard({ user }) {
-  const [symbols, setSymbols] = useState([]);
-  useEffect(() => {
-    const unsub = onSnapshot(doc(db, "users", user.uid), snap => {
-      setSymbols(snap.data()?.watchlist || []);
-    });
-    return unsub;
-  }, [user.uid]);
-
+export default function Dashboard() {
   return (
-    <div>
-      <Watchlist symbols={symbols} user={user} />
-      {symbols.map(sym => (
-        <div key={sym} style={{ marginTop: 20 }}>
-          <h2>{sym}</h2>
-          <RealTimeChart symbol={sym} />
-        </div>
-      ))}
+    <div style={{ padding: '2rem' }}>
+      <h1 style={{ textAlign: 'center' }}>Dashboard de Ações</h1>
+      <TradingViewChart />
     </div>
   );
 }
+
