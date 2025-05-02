89e27a70b106b94d11548ef7c9b18245638d029e
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-05-02 13:43:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Remova se não usar React
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
