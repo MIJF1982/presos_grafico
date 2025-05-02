c0bc1777c21e8bfe4fe35f6ab011ae3a72205725
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-05-02 12:46:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  },
-  root: '.',       // garante que procura index.html na raiz
 });
