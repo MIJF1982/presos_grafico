b0e75909359d4bcdc64221dcf0ab07db06dd9d8f
MIJF1982 <marlenejesusfreitas@hotmail.com>
2025-05-02 12:48:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
+import LoginPage from "../pages/LoginPage";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
   return (
